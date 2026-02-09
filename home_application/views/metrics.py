34ac3ac0a3acf11855febc0c88b9f6754e3bcb3f
tokyi <2398185624@qq.com>
2026-02-09 11:33:17 +0800

    refactor: 重构指标收集，改用 method/endpoint 标签并豁免登录检查

--- home_application/views/metrics.py
+++ home_application/views/metrics.py
@@ -2,14 +2,13 @@
 在 Django 中暴露 Prometheus 指标端点
 """
 
+from blueapps.account.decorators import login_exempt
 from django.http import HttpResponse
 from prometheus_client import Counter
 from rest_framework.views import APIView
 
 # 使用 prometheus_client 创建指标
-requests_total_omg = Counter(
-    "requests_total_omg", "Total number of HTTP requests", ["api_category", "api_name", "is_error"]  # 标签名称列表
-)
+requests_total_omg = Counter("requests_total_omg", "Total HTTP Requests", ["method", "endpoint"])
 
 
 class MetricsAPIView(APIView):
@@ -18,6 +17,7 @@ class MetricsAPIView(APIView):
     访问 /metrics 可以获取所有指标数据
     """
 
+    @login_exempt
     def get(self, request):
         requests_total_omg.labels(method=request.method, endpoint=request.path).inc()
         return HttpResponse("custom_metrics")
